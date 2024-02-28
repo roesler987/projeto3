e3c94df6131429be85500dcd6df0ba424f16fca4
jean <jkroesler@hotmail.com>
2024-02-28 10:54:45 -0300

    adicinado imgs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import logo from './imagens/logo2.svg';
 
 function App() {
   return (
-    <div>
-      <header>
-     
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+        <img src={logo} alt='logo'></img>
+        <p><strong>Entre</strong>Paginas</p>
+        </div>
       </header>
     </div>
   );
