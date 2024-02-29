a99203903898d3a4dad9e7c89adcceed41144752
jean <jkroesler@hotmail.com>
2024-02-29 15:57:10 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import Logo from './componentes/Logo';
+import perfil from './imagens/perfil2.svg';
+import sacola from './imagens/sacola2.svg'
+
+const textOptions = ['CATEGORIAS','FAVORITOS','MINHA ESTANTE'];
+const iconsHeader = [perfil,sacola];
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
-      </header>
+        <ul className='options'>
+          {textOptions.map( (text) => (
+            <li className='option'><p>{text}</p></li>
+          ) ) }
+        </ul>
+
+        <ul className='icons'>
+            {iconsHeader.map( (icons) => ( 
+            <li className='icon'><img src={icons}></img></li>
+          ) )}
+        </ul>
+        </header>
     </div>
   );
 }
